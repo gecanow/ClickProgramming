ea0779ef4fbc50a6bcb2a4db29e6228855679423
Gabrielle Ecanow <gecanow@gmail.com>
2016-08-01 10:49:50 -0500

    Set up storyboard

---
+++ Click/ActionCell.swift
@@ -0,0 +1,18 @@
+//
+//  ActionCell.swift
+//  Click
+//
+//  Created by Gaby Ecanow on 8/1/16.
+//  Copyright © 2016 Mobile Makers. All rights reserved.
+//
+
+import UIKit
+
+class ActionCell: NSObject {
+    
+    // nothing to do yet...
+    
+    // has a name for debugginh purposes
+    var name = "action"
+    
+}
--- Click/ViewController.swift
+++ Click/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var functions : [ActionCell] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    //=================================================
+    // SETUP & UPDATE THE TABLEVIEW
+    //=================================================
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return functions.count
     }
-
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        
+        // Configure the cell...
+        cell.textLabel?.text = "\(functions[indexPath.row].name) \(indexPath.row)"
+        
+        return cell
+    }
+    
+    //=================================================
+    // ALLOW FOR FUNCTIONS TO BE DELETED
+    //=================================================
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            functions.removeAtIndex(indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+    @IBAction func onTappedAddAction(sender: UIButton) {
+        
+        // first i call on the action sheet which returns a string to me
+        
+    }
+    
+    
+    
 }
 
